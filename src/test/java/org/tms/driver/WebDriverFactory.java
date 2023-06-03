fecf43eb89536a9e7ab5b5da5e8797e8449203ad
LiliYunusova <YunusovaLiliya90@yandex.ru>
2023-06-03 21:47:06 +0300

    first commit

---
+++ src/test/java/org/tms/driver/WebDriverFactory.java
@@ -0,0 +1,38 @@
+package org.tms.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverFactory {
+    private final static int WAIT_TIMEOUT_SECOND = 3;
+
+    private WebDriverFactory() {
+    }
+
+    public static WebDriver getWebDriver(){
+        WebDriver driver = null;
+            switch (System.getProperty("browser", "chrome")) {
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                }
+                case "opera": {
+                    WebDriverManager.operadriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                }
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(WAIT_TIMEOUT_SECOND, TimeUnit.SECONDS);
+            return driver;
+        }
+    }
